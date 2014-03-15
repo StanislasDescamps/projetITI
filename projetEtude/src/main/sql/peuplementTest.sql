0594a26e1d8ae21ccbd5febf47e8e3e8f4082899
Utilisateur <Utilisateur@Utilisateur-PC.home>
2014-03-15 19:19:33 +0100

    Modification bdd

--- projetEtude/src/main/sql/peuplementTest.sql
+++ projetEtude/src/main/sql/peuplementTest.sql
@@ -13,11 +13,11 @@ INSERT INTO `calendrier`(`idCalendrier`,`idEtudiant`,`dateEmission`) VALUES (2,2
 INSERT INTO `calendrier`(`idCalendrier`,`idEtudiant`,`dateEmission`) VALUES (3,3,'2014-02-24');
 
 -- Evenement
-INSERT INTO `evenement`(`idEvenement`,`idCommission`,`titreEvent`,`descriptionEvent`,`lieuEvent`,`dateDebut`,`dateFin`) VALUES (1,1,'Salon gastronomique','Viens gouter les saveur d Espagne �a va �tre trop bon','RR034','2014-02-24 12:00','2014-02-24 13:00');
-INSERT INTO `evenement`(`idEvenement`,`idCommission`,`titreEvent`,`descriptionEvent`,`lieuEvent`,`dateDebut`,`dateFin`) VALUES (2,2,'Soir�e Rock','Viens danser un rock endiabl� des 80s','Salle de sport','2014-02-26 19:45','2014-02-26 22:00');
+INSERT INTO `evenement`(`idEvenement`,`idCommission`,`titreEvent`,`descriptionEvent`,`lieuEvent`,`dateDebut`,`dateFin`) VALUES (1,1,'Salon gastronomique','Viens gouter les saveur d Espagne ca va etre trop bon','RR034','2014-02-24 12:00','2014-02-24 13:00');
+INSERT INTO `evenement`(`idEvenement`,`idCommission`,`titreEvent`,`descriptionEvent`,`lieuEvent`,`dateDebut`,`dateFin`) VALUES (2,2,'Soiree Rock','Viens danser un rock endiable des 80s','Salle de sport','2014-02-26 19:45','2014-02-26 22:00');
 
 -- Commission
-INSERT INTO `commission`(`idCommission`,`idEtudiant`,`idPole`,`nomCommission`,`descriptionCom`,`adresseLogo`) VALUES (1,2,1,'La Sauce','Le plaisir de manger entre �tudiants','img/bda.jpg');
+INSERT INTO `commission`(`idCommission`,`idEtudiant`,`idPole`,`nomCommission`,`descriptionCom`,`adresseLogo`) VALUES (1,2,1,'La Sauce','Le plaisir de manger entre etudiants','img/bda.jpg');
 INSERT INTO `commission`(`idCommission`,`idEtudiant`,`idPole`,`nomCommission`,`descriptionCom`,`adresseLogo`) VALUES (2,3,2,'Club Rock HEI','Cours de danse tous les mercredi soir','img/rock.jpg');
 
 -- pole
