5afa581807bb9e2df28bcfcf581675d5c6c76361
Utilisateur <Utilisateur@192.168.1.14>
2014-08-10 15:40:33 +0200

    Ajout d'un nouveau type d'objet Etudiant

--- projetEtude/src/main/java/hei/model/Etudiant.java
+++ projetEtude/src/main/java/hei/model/Etudiant.java
@@ -8,6 +8,7 @@ public class Etudiant {
 	private String passWord;
 	private String email;
 	private Integer idDroit;
+	private String motif;
 	private boolean firstConnexion;
 	
 	public Etudiant(Integer idetudiant, String nom, String prenom, String motdepass, String email, Integer iddroit, boolean first){
@@ -33,46 +34,73 @@ public class Etudiant {
 		this.email = email;
 		this.passWord = password;
 	}
-	
-	public Integer getIdetudiant() {
+	public Etudiant(String nom, String prenom, String email, String motif){
+		this.nomEtudiant = nom;
+		this.prenomEtudiant = prenom;
+		this.email = email;
+		this.motif = motif;
+	}
+
+	public Integer getIdEtudiant() {
 		return idEtudiant;
 	}
-	public void setIdetudiant(Integer idetudiant) {
-		this.idEtudiant = idetudiant;
+
+	public void setIdEtudiant(Integer idEtudiant) {
+		this.idEtudiant = idEtudiant;
 	}
+
 	public String getNomEtudiant() {
 		return nomEtudiant;
 	}
+
 	public void setNomEtudiant(String nomEtudiant) {
 		this.nomEtudiant = nomEtudiant;
 	}
+
 	public String getPrenomEtudiant() {
 		return prenomEtudiant;
 	}
+
 	public void setPrenomEtudiant(String prenomEtudiant) {
 		this.prenomEtudiant = prenomEtudiant;
 	}
+
 	public String getPassWord() {
 		return passWord;
 	}
+
 	public void setPassWord(String passWord) {
 		this.passWord = passWord;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public Integer getIdDroit() {
 		return idDroit;
 	}
+
 	public void setIdDroit(Integer idDroit) {
 		this.idDroit = idDroit;
 	}
+
+	public String getMotif() {
+		return motif;
+	}
+
+	public void setMotif(String motif) {
+		this.motif = motif;
+	}
+
 	public boolean isFirstConnexion() {
 		return firstConnexion;
 	}
+
 	public void setFirstConnexion(boolean firstConnexion) {
 		this.firstConnexion = firstConnexion;
 	}
