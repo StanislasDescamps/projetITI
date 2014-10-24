648609ee2915890a86e3f4788ed4253e7b5dca12
Utilisateur <Utilisateur@172.17.11.12>
2014-10-24 18:40:01 -0400

    Ajout des fonctions setModeration et setUnModeration Ajout des fonctions listerEvenementModere et listerEvenementNonModere

--- projetEtude/src/main/java/hei/dao/EvenementDao.java
+++ projetEtude/src/main/java/hei/dao/EvenementDao.java
@@ -8,6 +8,10 @@ public interface EvenementDao {
 
 	public List<Evenement> listerEvenement(); //Liste tous les évéments de la bdd
 	
+	public List<Evenement> listerEvenementNonModere(); //Liste tous les évéments non modérés de la bdd
+	
+	public List<Evenement> listerEvenementModere(); //Liste tous les évéments modérés de la bdd
+	
 	public void ajouterEvenement(Evenement evenement); //ajout d'un nouvel événement
 	
 	public void modifierEvenement(Integer idEvenement, Integer idCommission, String nomCommission, String nomPole , String titre, String description, String lieu, String dateDebut, String dateFin, String heureDebut, String heureFin);//modifie les informations d'un evenement
@@ -21,4 +25,9 @@ public interface EvenementDao {
 	public Evenement getEvenementByDate(String dateDebut); //Récupération des information d'un évément à partir de sa date
 
 	public void supprimerEvenement(Integer idEvenement); //Supprime l'évènement dont l'id est en parametre
+
+	public void setModeration(Integer idEvenement); //Permet la modération de l'événement dont l'id est en paramètre
+	
+	public void setUnModeration(Integer idEvenement); //Permet de retirer la modération de l'événement dont l'id est en paramètre
+
 }
